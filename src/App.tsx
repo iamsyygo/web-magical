3e944412ebb2240907947dfc325ac1714cefae59
李雨锋 <liyufeng@gz-yibo.com>
2024-09-29 10:27:17 +0800

    🎉 init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ConfigProvider } from 'antd';
+
+function App() {
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token，影响范围大
+          // colorPrimary: '#00b96b',
+          // borderRadius: 2,
+          // 派生变量，影响范围小
+          // colorBgContainer: '#f6ffed',
+        },
+      }}
+    ></ConfigProvider>
+  );
+}
+
+export default App;
---
+++ uno.config.ts
@@ -0,0 +1,16 @@
+// uno.config.ts
+import { defineConfig, presetAttributify, presetUno, transformerDirectives, } from 'unocss'
+import { presetIcons } from '@unocss/preset-icons';
+
+export default defineConfig({
+    content: { },
+    transformers: [
+        transformerDirectives()
+    ],
+    presets: [
+        presetIcons(),
+        presetUno(),
+        presetAttributify()
+    ],
+    rules:[]
+})
